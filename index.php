676b7bdf284199ef243212df895639530d634a0f
Leah Konma <leahkonma02@gmail.com>
2024-01-16 13:22:02 -0500

    added fat-free

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-echo "My Diner";
-g
+
+/* Leah Konma
+ * 1.26.24
+ * https://leahkonma.greenriverdev.com/328/diner/
+ * This is my CONTROLLER for the Diner app
+ */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once ('vendor/autoload.php');
+
+//instantiate Fat-Free framework (F3)
+$f3 = Base::instance(); //static method
+
+//define a default route
+$f3->route('GET /', function (){
+    echo "My Diner";
+});
+
+//run Fat-Free
+$f3->run(); //instance method
+
+
+
