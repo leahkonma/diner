b0fff140ba62cf849eac29c9af538cafd5da6626
Leah Konma <leahkonma02@gmail.com>
2024-02-06 13:07:30 -0500

    added F3 hives for meals and condiments

--- index.php
+++ index.php
@@ -1,84 +1,106 @@
 <?php
-
-/* Leah Konma
- * 1.26.24
- * https://leahkonma.greenriverdev.com/328/diner/
+/* Tina Ostrander
+ * January 2024
+ * https://tostrander.greenriverdev.com/328/diner/
  * This is my CONTROLLER for the Diner app
  */
 
-//Turn on error reporting
+// Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Require the autoload file
+// Require the autoload file
 require_once ('vendor/autoload.php');
 
-//instantiate Fat-Free framework (F3)
+// Instantiate Fat-Free framework (F3)
 $f3 = Base::instance(); //static method
 
-//define a default route
-$f3->route('GET /', function (){
-    //display a view page
+// Define a default route
+$f3->route('GET /', function() {
+    //echo "My Diner";
+
+    // Display a view page
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
-//define a breakfast route
-$f3->route('GET /breakfast', function (){
-    //display a view page
+// Define a breakfast route
+$f3->route('GET /breakfast', function() {
+    //echo "Breakfast";
+
+    // Display a view page
     $view = new Template();
     echo $view->render('views/breakfast-menu.html');
 });
 
-//define an order 1 route
+// Define a order form 1 route
 $f3->route('GET|POST /order1', function($f3) {
+    //echo "Order Form Part I";
 
-    //if the form has been posted
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //validate the data
+    // If the form has been posted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // Validate the data
         $food = $_POST['food'];
         $meal = $_POST['meal'];
 
-        //put the data in the session array
+        // Put the data in the session array
         $f3->set('SESSION.food', $food);
         $f3->set('SESSION.meal', $meal);
 
-        //redirect to order2 route
-        $f3->reroute('order2');
+        // Redirect to order2 route
+        $f3->reroute('order-form2');
     }
 
+    //add data to the F3 "hive"
+    $f3->set('meals', array('breakfast','lunch','dinner'));
+
+    // Display a view page
     $view = new Template();
     echo $view->render('views/order-form1.html');
+
 });
 
-//define an order form 2 route
-$f3->route('GET|POST /order2', function ($f3) {
+// Define a order form 2 route
+$f3->route('GET|POST /order2', function($f3) {
+    //echo "Order Form Part II";
 
-    //if the form has been posted
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // If the form has been posted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
         // Validate the data
-        if (isset($_POST['conds'])) {
+        if (isset($_POST['conds'])){
             $conds = implode(", ", $_POST['conds']);
-        } else {
+        }
+        else {
             $conds = "None selected";
         }
 
         // Put the data in the session array
         $f3->set('SESSION.conds', $conds);
+
         // Redirect to summary route
         $f3->reroute('summary');
+
     }
-    //display a view page
+
+    //add data to the F3 "hive"
+    $f3->set('conds', array('Ketchup','Mustard','Mayo','Sriracha'));
+
+    // Display a view page
     $view = new Template();
-    echo $view->render('views/order-form1.html');
+    echo $view->render('views/order-form2.html');
+
 });
 
-//define a summary route
-$f3->route('GET /summary', function () {
-    //display a view page
+// Define an order summary route
+$f3->route('GET /summary', function() {
+    //echo "Thank you for your order!";
+
+    // Display a view page
     $view = new Template();
     echo $view->render('views/order-summary.html');
-    });
+});
 
-//run Fat-Free
+// Run Fat-Free
 $f3->run(); //instance method
